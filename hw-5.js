92418c07c126b4bfaf179816a46c00b8ee232d87
AnnaKapaeva <anna-kapaeva1989@yandex.ru>
2023-06-23 23:19:46 +0300

    finished all hw-5

--- hw-5.js
+++ hw-5.js
@@ -1,72 +1,90 @@
-// // 1
+// 1
 
-// function min(a,b) {
-//     if (a<b) {
-//     return a;    
-//     } else {
-//     return b;  
-//     }
-// }
-// min(4,7);
+function min(a,b) {
+    if (a<b) {
+    return a;    
+    } else {
+    return b;  
+    }
+}
+min(4,7);
 
-// // 2
+// 2
 
-// function number() {
-//     let parity = prompt(question);
-//     if (prompt === n % 2 == 0) {
-//         alert('число четное');
-//     } else {
-//         alert('число нечетное');
-//     }
-// }
+function number() {
+    let parity = prompt(question);
+    if (prompt === n % 2 == 0) {
+        alert('число четное');
+    } else {
+        alert('число нечетное');
+    }
+}
 
-// // 3
+// 3
 
-// function result(с) {
-//     const i = c * 2;
-//     return i;
-// }
-// result (5);
+function result(с) {
+    const i = c * 2;
+    return i;
+}
+result (5);
 
-// // 4
+// 4
 
-// const age=prompt('Сколько вам лет?');
-// if (age<0) {
-//     console.log('Вы ввели неправильное значение');
-// } else if (age<=12) {
-//     console.log('Привет, Друг!');
-// } else {
-//     console.log('Добро пожаловать!');
-// }
+const age=prompt('Сколько вам лет?');
+if (age<0) {
+    console.log('Вы ввели неправильное значение');
+} else if (age<=12) {
+    console.log('Привет, Друг!');
+} else {
+    console.log('Добро пожаловать!');
+}
 
-// // 5
+// 5
 
-// function calculation(d,e) {
-//     if (isNaN(d) || isNaN(e)) {
-//         console.log('Одно или оба значения не являются числом');
-//     } else {
-//         console.log(d*e);
-//     }
-// }
+function calculation(d,e) {
+    if (isNaN(d) || isNaN(e)) {
+        console.log('Одно или оба значения не являются числом');
+    } else {
+        console.log(d*e);
+    }
+}
 
-// calculation(9,10);
+calculation(9,10);
 
-// 6 не поняла как сделать
+6 не поняла как сделать
 
-// 7
+7
 
-// const circle1 = {
-//     radius: 10,
-//     getArea() {
-//         return circle1.radius;
-//     }
-// };
-// console.log(circle1.getArea());
+const circle1 = {
+    radius: 10,
+    getArea() {
+        return circle1.radius;
+    }
+};
+console.log(circle1.getArea());
 
-// const circle2 = {
-//     radius: 20,
-//     getPerimetr() {
-//         return circle2.radius;
-//     }
-// };
-// console.log(circle2.getPerimetr());
+const circle2 = {
+    radius: 20,
+    getPerimetr() {
+        return circle2.radius;
+    }
+};
+console.log(circle2.getPerimetr());
+
+// 8
+
+function showTextButton() {
+    let userAnswer = prompt('Введите номер месяца');
+
+    if (userAnswer === 12 || 1 || 2) {
+        alert('Зима');
+    } else if (userAnswer === 3 || 4 || 5) {
+        alert('Весна');
+    } else if (userAnswer === 6 || 7 || 9) {
+        alert('Лето');  
+    } else if (userAnswer === 9 || 10 || 11) {
+        alert('Осень'); 
+    } else {
+        alert('Такого месяца не существует');
+    }
+}
